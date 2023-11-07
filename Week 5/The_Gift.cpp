829ac7fd3d729a59785db8a326eb4ddf5f5d1c7e
Md Mehadi Hasan <129536488+mehadi214100@users.noreply.github.com>
2023-11-07 23:05:18 +0600

    Add files via upload

---
+++ Week 5/The_Gift.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sy cout << "YES\n"
+#define sn cout << "NO\n"
+#define se cout << '\n'
+#define sp " "
+using namespace std;
+
+int main() {
+    int n,p,f;
+    cin>>n>>p>>f;
+    int ct = n+f;
+    if(ct>=p) sy;
+   else  sn;
+    return 0;
+}
+
+
