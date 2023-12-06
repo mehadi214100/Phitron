d7aa038cb436ee5411379d9c17fbcf46b4827f13
Md Mehadi Hasan <129536488+mehadi214100@users.noreply.github.com>
2023-12-06 20:35:47 +0600

    Add files via upload

---
+++ Week 6/numbertransformation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sy cout << "YES\n"
+#define sn cout << "NO\n"
+#define se cout << '\n'
+#define sp " "
+using namespace std;
+
+int main() {
+    int t = 1;
+    cin>>t;
+    while (t--)
+    {
+        int x,y; cin >> x>>y;
+        if(y%x==0){
+            cout << 1<<sp<<y/x << endl;
+        }else{
+            cout << 0<<sp<<0 << endl;
+        }
+        
+        
+    }
+    
+    return 0;
+}
