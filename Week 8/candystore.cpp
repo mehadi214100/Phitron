7982f117de3cf7d894e7f006de47bd5a21f3c8f2
Md Mehadi Hasan <129536488+mehadi214100@users.noreply.github.com>
2023-12-07 05:56:21 +0600

    Add files via upload

---
+++ Week 8/candystore.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sy cout << "YES\n"
+#define sn cout << "NO\n"
+#define se cout << '\n'
+#define sp " "
+using namespace std;
+
+int main() {
+    int t = 1;
+    cin>>t;
+    while (t--)
+    {
+        int x,y; cin >> x>>y;
+        if(y<=x) cout << y;
+        else cout << (y-x)*2+x;
+        cout<<endl;
+        
+        
+        
+    }
+    
+    return 0;
+}
---
+++ Week 8/q_distinct-oppositesum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sy cout << "YES\n"
+#define sn cout << "NO\n"
+#define se cout << '\n'
+#define sp " "
+using namespace std;
+
+int main() {
+    int t = 1;
+    cin>>t;
+    while (t--)
+    {
+        int n; cin >> n;
+        int mid = n/2;
+        for(int i = mid ; i>=1;i--){
+            cout<<i << " ";
+        }
+        for(int i = mid+1 ; i<=n;i++){
+            cout<<i << " ";
+        }
+        cout<< endl;
+        
+        
+    }
+    
+    return 0;
+}
