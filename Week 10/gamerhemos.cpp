14904ad2a21a4a318a9f62473519bdf5fb1efced
Md Mehadi Hasan <129536488+mehadi214100@users.noreply.github.com>
2023-12-07 19:29:35 +0600

    Add files via upload

---
+++ Week 10/gamerhemos.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sy cout << "YES\n"
+#define sn cout << "NO\n"
+#define se cout << '\n'
+#define sp " "
+using namespace std;
+
+int main() {
+    int t = 1;
+    cin>>t;
+    while (t--)
+    {
+        int n,H;
+        cin >> n>>H;
+        int arr[n];
+        for(int i= 0;i<n;i++) cin >> arr[i];
+        sort(arr,arr+n,greater<int>());
+        int ct = 0;
+        int sum = arr[0]+arr[1];
+        if(H%sum==0){
+            cout << (H/sum) *2<< endl;
+        }else{
+            int mx =  (H/sum) *2;
+            
+            if((H%sum)<=arr[0]) cout << mx+1<< endl;
+            else cout << mx+2<< endl;
+        }
+    }
+
+    return 0;
+}
