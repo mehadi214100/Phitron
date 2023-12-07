71aca63d2910ac6659b7a15a51913edb3dcf1694
Md Mehadi Hasan <129536488+mehadi214100@users.noreply.github.com>
2023-12-07 14:49:06 +0600

    Add files via upload

---
+++ Week 9/matrixdiff.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sy cout << "YES\n"
+#define sn cout << "NO\n"
+#define se cout << '\n'
+#define sp " "
+using namespace std;
+
+int main() {
+    int t = 1;
+    cin>>t;
+    while (t--)
+    {
+        int n;cin >> n;
+        int num = n*n;
+        
+        for(int i= 1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                cout << num << " ";
+                num -= 2;
+                if(num<=0){
+                    num = n*n -1;
+                }
+            }
+            cout  << endl;
+        }
+        cout  << endl;
+        
+        
+
+
+
+    }
+
+    return 0;
+}
